ac1402c6465bc14d40d2743ce0b2d995d5198521
sanjaygit1 <sanjay1222babu@gmail.com>
2024-07-03 09:59:07 +0530

    1

---
+++ Vtiger_Project/src/test/java/testNg/SampleTest.java
@@ -0,0 +1,26 @@
+package testNg;
+
+import org.testng.annotations.Test;
+@Test
+
+public class SampleTest {
+
+	public void createProduct()
+	{
+	System.out.println("product created");	
+	}
+
+	
+	public void modifyProduct()
+	{
+		System.out.println("product modified");
+	}
+	
+
+	public void deleteProduct()
+	{
+		System.out.println("product deleted");
+	}
+	
+	//Bye
+}
